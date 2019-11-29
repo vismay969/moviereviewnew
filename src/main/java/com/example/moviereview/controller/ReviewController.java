80756231f70fbb7cdbd12be6fbba56c879367331
vismay969 <vismayrshah@gmail.com>
2019-11-29 12:47:40 +0530

    Ver 1.1

--- src/main/java/com/example/moviereview/controller/ReviewController.java
+++ src/main/java/com/example/moviereview/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.moviereview.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,11 @@ public class ReviewController {
     @GetMapping(value = "/message")
     public String getMessage() {
         return "Hello World";
+    }
+
+    @GetMapping(value = "/message/{name}")
+    public String anotherMessage(@PathVariable("name") String name) {
+
+        return "Hello " + name;
     }
 }
